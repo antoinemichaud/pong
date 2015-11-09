5bb0244ec8d905eef344fcb18624f6af57cd9b2c
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-11-09 21:33:11 +0100

    handle timeout

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ var app = express();
 app.get('/sendMe/:addressToCall', function (req, res) {
   var addressToCall = req.params.addressToCall;
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  requestAsync("http://" + ip + ':3000/' + addressToCall).then(function (result) {
+  requestAsync("http://" + ip + ':3000/' + addressToCall, {timeout: 1500}).then(function (result) {
     res.send(result.body);
+  }).catch(function() {
+    res.send("timeout ! but ip was " + ip);
   });
 });
 
 app.get('/success', function(req, res) {
   res.send("success");
+});
+
+app.get('/timeout', function(req, res) {
+  setTimeout(function() {res.send("success")}, 2500);
 });
 
 app.get('/httpsuccess', function (req, res) {
