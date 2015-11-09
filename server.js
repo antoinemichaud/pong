99669cb7e3ecd0845b2db93b1823f737d83c6001
Antoine MICHAUD <antoinemichaud34@gmail.com>
2015-11-09 22:53:21 +0100

    ping

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var Promise = require('bluebird');
 var _ = require('lodash');
-var ping = require('ping-wrapper2');
 var requestAsync = Promise.promisify(require('request'));
 
 var app = express();
@@ -11,26 +10,36 @@ app.get('/sendMe/:addressToCall', function (req, res) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   requestAsync("http://" + ip + ':3000/' + addressToCall, {timeout: 1500}).then(function (result) {
     res.send(result.body);
-  }).catch(function() {
+  }).catch(function () {
     res.send("timeout ! but ip was " + ip);
   });
 });
 
 app.get('/pingMe/:ipAddress', function (req, res) {
   var ipAddress = req.params.ipAddress;
-  var exec = ping("google.com", { count: 1 });
-  exec.on("exit", function(data){
-    // { no: 1, bytes: 64, time: 54, ttl: 1 }
-    res.send(data);
+  var ping = require('ping');
+
+  var hosts = [ipAddress];
+
+  hosts.forEach(function (host) {
+    ping.promise.probe(host)
+      .then(function (pingRes) {
+        res.send(pingRes);
+      })
+      .catch(function (error) {
+        res.send(error);
+      });
   });
 });
 
-app.get('/success', function(req, res) {
+app.get('/success', function (req, res) {
   res.send("success");
 });
 
-app.get('/timeout', function(req, res) {
-  setTimeout(function() {res.send("success")}, 2500);
+app.get('/timeout', function (req, res) {
+  setTimeout(function () {
+    res.send("success")
+  }, 2500);
 });
 
 app.get('/httpsuccess', function (req, res) {
